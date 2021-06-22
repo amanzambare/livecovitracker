e34e917fa3a69aaa68e8ec7fa4fb2a4fb48109b1
amanzambare <76624676+amanzambare@users.noreply.github.com>
2021-06-23 00:19:02 +0530

    commit

--- src/covid.js
+++ src/covid.js
@@ -20,61 +20,71 @@ const Covid = () => {
   }, [])
   return (
     <>
-    <section>
+    <center>
+
     <h1>Live</h1>
       <h1>Covid-19 coronavirus tracking</h1>
-       <ul>
-          <li className="card">
-                <div className="card_main">
-                    <div className="card_inner">
+       
+          
                         <p className="card_name"><span>OUR</span> COUNTRY</p>
                         <p className="card_total card_small">INDIA</p>
-                    </div>
-                </div>      
-         </li>   
+    </center>
+                    
+           
+         
+         <div className="cards"> 
+                <li className="card">
+                        <div className="card_main" >
+                            <div className="card_inner">
+                                <p className="card_name"><h1><span>TOTAL</span> RECOVERED</h1></p>
+                                <p className="card_total card_small"><h3>{data.recovered}</h3></p>
+                            </div>
+                        </div>      
+                </li>
+         </div>
+         <div className="cards">
          <li className="card">
                 <div className="card_main">
                     <div className="card_inner">
-                        <p className="card_name"><span>TOTAL</span> RECOVERED</p>
-                        <p className="card_total card_small">{data.recovered}</p>
+                        <p className="card_name"><h1> CONFIRMED</h1></p>
+                        <p className="card_total card_small"><h3>{data.confirmed}</h3></p>
                     </div>
                 </div>      
          </li>
-         <li className="card">
+         </div>
+        <div className="cards">
+                 <li className="card">
                 <div className="card_main">
                     <div className="card_inner">
-                        <p className="card_name"><span></span> CONFIRMED</p>
-                        <p className="card_total card_small">{data.confirmed}</p>
+                        <p className="card_name"><h1>TOTAL DEATHS</h1></p>
+                        <p className="card_total card_small"><h3>{data.deaths}</h3></p>
                     </div>
                 </div>      
          </li>
+         </div>
+         <div className="cards">
          <li className="card">
                 <div className="card_main">
                     <div className="card_inner">
-                        <p className="card_name"><span>TOTAL</span> DEATHS</p>
-                        <p className="card_total card_small">{data.deaths}</p>
+                        <p className="card_name"><h1><span>TOTAL</span> ACTIVE</h1></p>
+                        <p className="card_total card_small"><h3>{data.active}</h3></p>
                     </div>
                 </div>      
          </li>
+         </div>
+         <div className="cards">
          <li className="card">
                 <div className="card_main">
                     <div className="card_inner">
-                        <p className="card_name"><span>TOTAL</span> ACTIVE</p>
-                        <p className="card_total card_small">{data.active}</p>
+                        <p className="card_name"><h1><span>LAST</span> UPDATED</h1></p>
+                        <p className="card_total card_small"><h3>{data.lastupdatedtime}</h3></p>
                     </div>
                 </div>      
          </li>
-         <li className="card">
-                <div className="card_main">
-                    <div className="card_inner">
-                        <p className="card_name"><span>LAST</span> UPDATED</p>
-                        <p className="card_total card_small">{data.lastupdatedtime}</p>
-                    </div>
-                </div>      
-         </li> 
-       </ul> 
+         </div> 
+      
 
-    </section>
+       
       
     </>
   )
